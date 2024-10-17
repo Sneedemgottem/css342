e980594da93f7ef4f99255dae19918e156c587ea
sneedemgottem <raa02@protonmail.com>
2024-10-16 17:57:54 -0700

    Fixed cmake for catch2 to include main function

--- program1/test_time_span.cpp
+++ program1/test_time_span.cpp
@@ -1,2 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "time_span.h"
+#include <catch2/catch_test_macros.hpp>
+
+int giveone() {
+    return 1;
+}
+
+TEST_CASE("Test Catch2 is working", "[giveone]") {
+    REQUIRE( giveone() == 1);
+}

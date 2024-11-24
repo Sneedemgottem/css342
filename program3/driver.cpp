451e8bf681894d64f9f6ddbe771ce2917783e524
Razvan Anghel <csneed@Razvans-Air.home>
2024-11-24 02:12:32 -0800

    Finished program 3. Works with example code and no memory leaks

--- program3/driver.cpp
+++ program3/driver.cpp
@@ -2,25 +2,10 @@
 #include <string>
 using std::string;
 #include "list342.h"
+#include "child.h"
 
 int main() {
-    string the = "the";
-    string fox = "fox";
-    string jumped = "jumped";
-    string jumped2 = "jumped";
-    string high = "high";
-    string the2 = "the";
-
-    List342<string> list;
-    list.Insert(&the);
-    list.Insert(&fox);
-    list.Insert(&jumped);
-    list.Insert(&high);
-    list.Insert(&jumped2);
-    list.Insert(&the);
-    list.Insert(&fox);
-
-    list.PrintList();
-
-    return 0;
+  List342<Child> list;
+  list.BuildList("children.txt");
+  list.PrintList();
 }
